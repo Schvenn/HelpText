6d1b07f566e164fd29d9a07f27a797dfeb06cafb
Schvenn <42421399+Schvenn@users.noreply.github.com>
2025-05-18 01:37:13 -0400

    V1.8
    The module will now resolve the names of aliases and functions, as well as modules and scripts, in order to load the appropriate help menu.

--- HelpText.psm1
+++ HelpText.psm1
@@ -4,30 +4,37 @@ $powershell = Split-Path $profile
 if ($script -eq "helptext") {$script = $PSCommandPath}
 
 function scripthelp ($section) {# (Internal) Generate the help sections from the comments section of the script.
-""; Write-Host -ForegroundColor Yellow ("-" * 100); $pattern = "(?ims)^## ($section.*?)(##|\z)"; $match = [regex]::Match($scripthelp, $pattern); $lines = $match.Groups[1].Value.TrimEnd() -split "`r?`n", 2; Write-Host $lines[0] -ForegroundColor Yellow; Write-Host -ForegroundColor Yellow ("-" * 100)
-if ($lines.Count -gt 1) {$lines[1] | Out-String | Out-Host -Paging}; Write-Host -ForegroundColor Yellow ("-" * 100)}
+""; Write-Host -f yellow ("-" * 100); $pattern = "(?ims)^## ($section.*?)(##|\z)"; $match = [regex]::Match($scripthelp, $pattern); $lines = $match.Groups[1].Value.TrimEnd() -split "`r?`n", 2; Write-Host $lines[0] -f yellow; Write-Host -f yellow ("-" * 100)
+if ($lines.Count -gt 1) {$lines[1] | Out-String | Out-Host -Paging}; Write-Host -f yellow ("-" * 100)}
 
 # Present a menu of help-enabled scripts if no parameter was provided.
 if (-not $script) {$basedir = Split-Path -Parent $profile; $profileFile = Get-Item -Path $PROFILE -ErrorAction SilentlyContinue; $found = @()
 if ($profileFile -and (Get-Content -Raw $profileFile.FullName -ErrorAction SilentlyContinue) -match '##>\s*$') {$found += $profileFile}
 $found += Get-ChildItem -Path $basedir -Recurse -Include *.ps1,*.psm1 -ErrorAction SilentlyContinue | Where-Object {($_ | Get-Content -Raw -ErrorAction SilentlyContinue) -match '##>\s*$'}
 if (-not $found) {""; return}
-Write-Host "`nAvailable Help-Enabled Modules:`n" -ForegroundColor Cyan
+Write-Host "`nAvailable Help-Enabled Modules:`n" -f cyan
 for ($i = 0; $i -lt $found.Count; $i++) {"{0}: {1}" -f ($i + 1), ([System.IO.Path]::GetFileNameWithoutExtension($found[$i].FullName))}
 $choice = Read-Host "`nEnter the number of the module to view"
 if ($choice -match '^\d+$') {$index = [int]$choice; if ($index -ge 1 -and $index -le $found.Count) {$script = $found[$index - 1].FullName} else {"" ; return}} else {"" ; return}}
 
-# Call the help menu.
+# Find the help menu from an alias or function.
+$target = Get-Command -Name $script -ErrorAction SilentlyContinue
+if ($target) {if ($target.CommandType -in 'Alias', 'Function') {if ($target.Module -and $target.Module.Path) {$script = $target.Module.Path}
+elseif ($target.ScriptBlock.File) {$script = $target.ScriptBlock.File}
+elseif ($target.ResolvedCommand -and $target.ResolvedCommand.Source) {$script = $target.ResolvedCommand.Source}
+else {return}}}
+
+# Find the help menu from a script or module path.
 $resolved = Resolve-Path $script -ErrorAction SilentlyContinue
 if (-not $resolved -or $resolved -notmatch ".psm?1") {$basename = $script; $found = Get-ChildItem -Path $powershell -Recurse -File -Include *.ps1, *.psm1 -ErrorAction SilentlyContinue | Where-Object {[System.IO.Path]::GetFileNameWithoutExtension($_.Name) -ieq $basename}
 if ($found.Count -eq 1) {$script = $found[0].FullName} else {""; return}}
 
-if (-not (Resolve-Path $script -ErrorAction SilentlyContinue)) {return}
+# Create and present help menu.
 $scripthelp = Get-Content -Raw -Path $script; $sections = [regex]::Matches($scripthelp, "(?im)^## (.+?)(?=\r?\n)")
 if (-not $sections) {Write-Host -f cyan "`nThis is not a HelpText enabled file.`n"; return}
-if ($sections.Count -eq 1) {cls; Write-Host "$([System.IO.Path]::GetFileNameWithoutExtension($script)) Help:" -ForegroundColor Cyan; scripthelp $sections[0].Groups[1].Value; ""; return}
+if ($sections.Count -eq 1) {cls; Write-Host "$([System.IO.Path]::GetFileNameWithoutExtension($script)) Help:" -f cyan; scripthelp $sections[0].Groups[1].Value; ""; return}
 $selection = $null
-do {cls; Write-Host "$([System.IO.Path]::GetFileNameWithoutExtension($script)) Help Sections:`n" -ForegroundColor Cyan; for ($i = 0; $i -lt $sections.Count; $i++) {
+do {cls; Write-Host "$([System.IO.Path]::GetFileNameWithoutExtension($script)) Help Sections:`n" -f cyan; for ($i = 0; $i -lt $sections.Count; $i++) {
 "{0}: {1}" -f ($i + 1), $sections[$i].Groups[1].Value}
 if ($selection) {scripthelp $sections[$selection - 1].Groups[1].Value}
 $input = Read-Host "`nEnter a section number to view"
@@ -57,12 +64,12 @@ The parameter allows -help to be an option for the script and the "if" section e
 If however, you want to keep the help menu completely self-contained, perhaps because you want to share your script publicly, then use this body for the help script, instead:
 
 if ($help) {function scripthelp ($section) {# (Internal) Generate the help sections from the comments section of the script.
-""; Write-Host -ForegroundColor Yellow ("-" * 100); $pattern = "(?ims)^# # ($section.*?)(# #|\z)"; $match = [regex]::Match($scripthelp, $pattern); $lines = $match.Groups[1].Value.TrimEnd() -split "`r?`n", 2; Write-Host $lines[0] -ForegroundColor Yellow; Write-Host -ForegroundColor Yellow ("-" * 100)
-if ($lines.Count -gt 1) {$lines[1] | Out-String | Out-Host -Paging}; Write-Host -ForegroundColor Yellow ("-" * 100)}
+""; Write-Host -f yellow ("-" * 100); $pattern = "(?ims)^# # ($section.*?)(# #|\z)"; $match = [regex]::Match($scripthelp, $pattern); $lines = $match.Groups[1].Value.TrimEnd() -split "`r?`n", 2; Write-Host $lines[0] -f yellow; Write-Host -f yellow ("-" * 100)
+if ($lines.Count -gt 1) {$lines[1] | Out-String | Out-Host -Paging}; Write-Host -f yellow ("-" * 100)}
 $scripthelp = Get-Content -Raw -Path $PSCommandPath; $sections = [regex]::Matches($scripthelp, "(?im)^# # (.+?)(?=\r?\n)")
-if ($sections.Count -eq 1) {cls; Write-Host "$([System.IO.Path]::GetFileNameWithoutExtension($PSCommandPath)) Help:" -ForegroundColor Cyan; scripthelp $sections[0].Groups[1].Value; ""; return}
+if ($sections.Count -eq 1) {cls; Write-Host "$([System.IO.Path]::GetFileNameWithoutExtension($PSCommandPath)) Help:" -f cyan; scripthelp $sections[0].Groups[1].Value; ""; return}
 $selection = $null
-do {cls; Write-Host "$([System.IO.Path]::GetFileNameWithoutExtension($PSCommandPath)) Help Sections:`n" -ForegroundColor Cyan; for ($i = 0; $i -lt $sections.Count; $i++) {
+do {cls; Write-Host "$([System.IO.Path]::GetFileNameWithoutExtension($PSCommandPath)) Help Sections:`n" -f cyan; for ($i = 0; $i -lt $sections.Count; $i++) {
 "{0}: {1}" -f ($i + 1), $sections[$i].Groups[1].Value}
 if ($selection) {scripthelp $sections[$selection - 1].Groups[1].Value}
 $input = Read-Host "`nEnter a section number to view"
